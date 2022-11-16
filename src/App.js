2beaa17b8d8998f82f07fd0fb12416d413855c75
Kazem <mohammadkazem4000@outlook.com>
2022-11-16 21:26:08 +0330

    localizations are created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { t } from 'i18next';
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// page uses the hook
+function Page() {
+const { t, i18n } = useTranslation();
+
+const changeLanguage = (lng) => {
+i18n.changeLanguage(lng);
+};
+document.body.dir = t('dir')
+return (
+<div className="App">
+  <div className="App-header">
+    
+  
+    <button type="button" onClick={()=> changeLanguage('de')}>
+      de
+    </button>
+    <button type="button" onClick={()=> changeLanguage('en')}>
+      en
+    </button>
+        < button type = "button"
+        onClick = {
+            () => changeLanguage('fa')
+          } >
+          فارسی </button>
+  </div>
+   < div > {
+     t('loading...')} </div>
+  <div className="App-intro">
+
+  </div>
+  <div>{t('description.part2')}</div>
+</div>
+);
 }
 
-export default App;
+// loading component for suspense fallback
+const Loader = () => (
+<div className="App">
+ 
+  <div>{t('loading...')}</div>
+</div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+return (
+<Suspense fallback={<Loader />}>
+<Page />
+</Suspense>
+)}
